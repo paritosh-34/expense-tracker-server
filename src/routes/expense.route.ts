6569959f633550ed3bddfb7ea7ec763517fdef36
Paritosh Batish <batishparitosh2@gmail.com>
2021-10-21 16:53:29 +0530

    added missing slash in routes

--- src/routes/expense.route.ts
+++ src/routes/expense.route.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import express from 'express';
 import expenseController from '@controllers/expense.controller';
 import { authenticate } from '@utils/auth.util';
 
 const router = express.Router();
 
-router.post('create', authenticate, expenseController.create);
-router.get('all', authenticate, expenseController.all);
+router.post('/create', authenticate, expenseController.create);
+router.get('/all', authenticate, expenseController.all);
 
 export default router;
